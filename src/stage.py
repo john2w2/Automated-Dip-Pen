1fb1a1b08f060d66fca4c27994b8a74ab944bfca
Edward Zhang <61752925+zeddie888@users.noreply.github.com>
2022-08-23 11:48:39 -0700

    New async code which fixes serial bug (#6)
    * Create Chip class
    
    * Finish Chip class
    
    * Create Plate class
    
    * Create Stage class, updated docs
    
    * Add stage position and serial commands
    
    * Test stage movements
    
    * Rewrite position determining functions for stage, based on Prior's own recommendations
    
    * Fix major movement command bug
    
    Movement commands sent to Prior stage will return 'R' when the movement
    itself is finished. Implemented mock version of async to wait--in case
    of movement-- until 'R' is seen
    
    This fixes the bug where leftover bytes in stream collide with later
    reads

--- src/stage.py
+++ src/stage.py
@@ -1,13 +1,15 @@
 import serial
 from plate import *
 from chip import *
+import time
 
 # The "Stage" really encompasses the whole setup on top of the stage- Chip, Plate, and microscope stage
 
 
 class Stage:
-    def __init__(self, plate: Plate, chip: Chip, port="COM6", baudrate=9600, stepSize=0.04):
-        self.ser = serial.Serial(port, baudrate, timeout=0.1)
+    # TODO: change the default values for firstWellPos, they are bogus
+    def __init__(self, plate: Plate, chip: Chip, port="COM6", baudrate=9600, timeout=0.1, stepSize=0.04):
+        self.ser = serial.Serial(port, baudrate, timeout=timeout)
         self.plate = plate
         self.chip = chip
 
@@ -18,6 +20,24 @@ class Stage:
 
         # TODO: used to have self.plateInitX, plateInitY
         # We should standardize that initial reference well is A1, since there is no guarantee which well plate is being used
+        
+
+    def setOrigin(self):
+        # TODO: make a note of this in the official docs
+        # Since (0, 0) is bottom right, stage up => y decreases, stage left => x decreases
+
+        self.writeRead("G,0,0", isMoveCmd=True) # Move stage to absolute 0,0 position
+        self.writeRead("P,0,0,0") # Define this location as origin
+        # Prior advises that SIS only be used upon first installation
+
+    
+    # Set coordinates for well A1 on stage
+    # Again, we justify that the "stage" encapsulates the actual microscope stage + well/chip on top of it
+    def setFirstWellPos(self):
+        # Assume user has positioned printer head above center of well A1
+        stagePos = self.getStageXY()
+        self.firstWellPos = tuple(stagePos)
+
 
     def getStageX(self) -> int:
         """Gets the X position of the stage (in number of motor steps)
@@ -35,19 +55,39 @@ class Stage:
         res = self.writeRead("PY")
         return int(res)
 
+
+    def getStageXY(self):
+        res = self.writeRead("P")
+        res = res.split(",")
+        res = [int(x) for x in res]
+        return res[:-1] # ignore z
+
+    # Prior's manual recommends this style of checking for motion
+    def isMoving(self) -> bool:
+        start = self.getStageXY()
+        end = self.getStageXY()
+        return not(start == end)        
+
+
         
     # Expecting no response
-    def write(self, cmd: str):
-        cmd = f"{cmd}\r"
-        self.ser.write(cmd.encode())
+    # def write(self, cmd: str):
+    #     cmd = f"{cmd}\r"
+    #     self.ser.write(cmd.encode())
 
 
     # Expecting response
-    def writeRead(self, cmd: str) -> str:
+    def writeRead(self, cmd: str, isMoveCmd=False) -> str:
         cmd = f"{cmd}\r"
         self.ser.write(cmd.encode())
-        res = self.ser.readline().decode()
-        return res
+        # wait until we get back "R" if movement command
+        if isMoveCmd:
+            res = self.ser.readline()
+            while(not(b'R' in res)):
+                res = self.ser.readline()
+            return res.decode()
+        else:
+            return self.ser.readline().decode()
     
 
     def close(self):
